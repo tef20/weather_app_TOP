150b98f9722d6f7d9d93146a0f53fd5392f24697
Chris Telford <christelford@hotmail.co.uk>
2021-11-04 22:38:43 +0000

    update css reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import './style.css';
+import "./style.css";
+import "./reset.css";
 
-const title = document.createElement('h1');
-title.textContent = 'hello';
+const title = document.createElement("input");
+title.textContent = "hello";
+
 document.body.appendChild(title);
+
+async function asyfun() {
+  const timeOut = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("FUN"), 4000);
+  });
+  const result = await timeOut;
+}
